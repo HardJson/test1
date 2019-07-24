32e440afb643a7c09956f34f2105764758a875c8
HardJson <tony1353855@163.com>
2019-07-24 23:46:05 +0800

    bywp dsg

---
+++ src/main/java/com/alipay/dsg/DsgApplication.java
@@ -0,0 +1,13 @@
+package com.alipay.dsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsgApplication.class, args);
+    }
+
+}
